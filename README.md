dcdf9b2fd34f4609c8b400c2173fcf49737e46ca
r1cc4r2o <riccardotedoldi@rickbook.homenet.telecomitalia.it>
2023-12-17 16:49:41 +0100

    feature added to the CSV utils.py

--- README.md
+++ README.md
@@ -3,21 +3,21 @@ We're developing a way for managers and professionals to find what they need.
 
 ## KB
 Look for a Job features:
-- [ ] search by location
-- [ ] remote vs hybrid vs in office
-- [ ] willingness to relocate
-- [ ] company size
+- [x] search by location
+- [x] remote (0) vs hybrid (1) vs in office (2)
+- [x] willingness to relocate (1) vs not willing (0)
+- [x] current company in which is working
 - [ ] hiring manager
-- [ ] example of average work day
+- [x] example of part time full time
 - [ ] benefits
-- [ ] company name
-- [ ] salary range
-- [ ] position/role/level
+- [x] company name
+- [x] salary range
+- [x] position/role/level
 - [ ] visa sponsorship
 - [x] job title
 - [x] person description
-- [ ] education
-- [ ] experience (number years)
+- [x] education
+- [x] experience (number years)
 - [x] name
 - [x] surname
 - [x] email (name.surname@xyz.edu)
--- utils/utils.py
+++ utils/utils.py
@@ -134,6 +134,119 @@ def get_5_random_skills(name_surname_gender_mansion):
 ############################################################################################################
 ############################################################################################################
 
+def get_random_city_country(name_surname_gender_mansion):
+    """
+    Returns random city and country for each individual
+    
+    params: df with [name, surname, mansion, mansion_id, description, salary]
+    :return: two lists of random city and country for each individual
+    """
+    city_country = [random.sample(config.CITIES_COUNTRY, 1)[0] for _ in range(len(name_surname_gender_mansion))]
+    return [city for city, _ in city_country], [country for _, country in city_country]
+
+############################################################################################################
+############################################################################################################
+
+def get_remote_hybrid_office(name_surname_gender_mansion):
+    """
+    Returns random remote, hybrid or office for each individual
+    
+    params: df with [name, surname, mansion, mansion_id, description, salary]
+    :return: a list of remote, hybrid or office for each individual
+        # 0: remote
+        # 1: hybrid
+        # 2: office
+    """
+    return [random.randint(0, 2) for _ in range(len(name_surname_gender_mansion))]
+
+############################################################################################################
+############################################################################################################
+
+def get_relocation(name_surname_gender_mansion):
+    """
+    Returns random willin to relocate for each individual
+    
+    params: df with [name, surname, mansion, mansion_id, description, salary]
+    :return: a list of willin to relocate for each individual
+        # 0: no
+        # 1: yes
+    """
+    return [random.randint(0, 1) for _ in range(len(name_surname_gender_mansion))]
+
+############################################################################################################
+############################################################################################################
+
+def get_name_companies(name_surname_gender_mansion):
+    """
+    Returns random name of companies for each individual
+    
+    params: df with [name, surname, mansion, mansion_id, description, salary]
+    :return: a list of name of companies for each individual
+    """
+    return [random.sample(config.NAME_COMPANIES_SECTORS, 1)[0][0] for _ in range(len(name_surname_gender_mansion))]
+
+############################################################################################################
+############################################################################################################
+
+def get_part_full_time(name_surname_gender_mansion):
+    """
+    Returns random part or full time for each individual
+    
+    params: df with [name, surname, mansion, mansion_id, description, salary]
+    :return: a list of part or full time for each individual
+        # 0: part time
+        # 1: full time
+    """
+    return [random.randint(0, 1) for _ in range(len(name_surname_gender_mansion))]
+
+############################################################################################################
+############################################################################################################
+
+def get_level_position(name_surname_gender_mansion):
+    """
+    Returns random junior or senior for each individual
+    
+    params: df with [name, surname, mansion, mansion_id, description, salary]
+    :return: a list of junior or senior for each individual
+        # 0: junior
+        # 1: senior
+    """
+    return [random.randint(0, 1) for _ in range(len(name_surname_gender_mansion))]
+
+############################################################################################################
+############################################################################################################
+
+def get_years_experience(name_surname_gender_mansion):
+    """
+    Returns random years of experience for each individual
+    
+    params: df with [name, surname, mansion, mansion_id, description, salary]
+    :return: a list of years of experience for each individual
+
+    """
+    return [random.randint(0, 20) for _ in range(len(name_surname_gender_mansion))]
+
+############################################################################################################
+############################################################################################################
+
+def get_education(name_surname_gender_mansion):
+    """
+    Returns random years of experience for each individual
+    
+    params: df with [name, surname, mansion, mansion_id, description, salary]
+    :return: a list that indicates the education level for each individual
+        # 0: high school
+        # 1: bachelor
+        # 2: master
+        # 3: phd
+        # 4: postdoc
+
+    """
+    return [random.randint(0, 4) for _ in range(len(name_surname_gender_mansion))]
+
+############################################################################################################
+############################################################################################################
+
 def fill_all_slots_salary(name_surname_gender_mansion):
     """
     Fill all slots in description with random attributes in the dictionary
@@ -158,5 +271,33 @@ def fill_all_slots_salary(name_surname_gender_mansion):
     
     # add skills
     name_surname_gender_mansion['skills'] = get_5_random_skills(name_surname_gender_mansion)
+    
+    # add city and country
+    city, country = get_random_city_country(name_surname_gender_mansion)
+    name_surname_gender_mansion['city'] = city
+    name_surname_gender_mansion['country'] = country
+    
+    del city, country
+    
+    # add remote, hybrid or office
+    name_surname_gender_mansion['remote_hybrid_office'] = get_remote_hybrid_office(name_surname_gender_mansion)
+    
+    # add relocation
+    name_surname_gender_mansion['relocation'] = get_relocation(name_surname_gender_mansion)
+    
+    # add name of companies
+    name_surname_gender_mansion['current_company'] = get_name_companies(name_surname_gender_mansion)
+    
+    # add part or full time
+    name_surname_gender_mansion['part_full_time'] = get_part_full_time(name_surname_gender_mansion)
+    
+    # add level position
+    name_surname_gender_mansion['junior_senior'] = get_level_position(name_surname_gender_mansion)
+    
+    # add years of experience
+    name_surname_gender_mansion['experience_years'] = get_years_experience(name_surname_gender_mansion)
+    
+    # add education
+    name_surname_gender_mansion['level_education'] = get_education(name_surname_gender_mansion)
     
     return name_surname_gender_mansion
